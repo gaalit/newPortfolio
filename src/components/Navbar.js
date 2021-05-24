835258c849c1eb1b0c2a150ec5e3b85f868c7739
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-05-24 10:36:07 -0400

    added react router

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={logo} alt="logo..."></img>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
